d919d2806304812d31866772835062ad5df5e93d
ZubairAsif <zubair@softpyramid.com>
2022-10-12 18:26:42 +0500

    Models created for independent forms.

---
+++ app/Models/SessionExam.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SessionExam extends Model
+{
+    use HasFactory;
+}
