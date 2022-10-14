7c76b9f8ccaa1bf0f08f1b26c768ff4b90b4923d
mrizwan1122 <gujjar19014@gmail.com>
2022-10-14 12:52:48 +0500

    city resource crud created

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['country_id', 'name', 'zip_code'];
+
+    public function country(){
+		return $this->belongsTo(Country::class);
+	}
 }
